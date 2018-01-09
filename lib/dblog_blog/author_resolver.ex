c8407bf230615132179ef50684ba5df3f9a019b7
Daniel Blanco <daniel.blancorojas@gmail.com>
2018-01-08 23:41:34 -0600

    Removing deprecated resolvers

--- lib/dblog_blog/author_resolver.ex
+++
@@ -1,7 +0,0 @@
-defmodule Dblog.Accounts.AuthorResolver do
-  alias Dblog.Blog
-
-  def all(_args, _info) do
-    {:ok, Blog.list_authors()}
-  end
-end
--- lib/dblog_blog/post_resolver.ex
+++
@@ -1,7 +0,0 @@
-defmodule Dblog.Blog.PostResolver do
-  alias Dblog.Blog
-
-  def all(_args, _info) do
-    {:ok, Blog.list_posts()}
-  end
-end
